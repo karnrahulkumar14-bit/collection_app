2a027c6bac817f298e58cedf1eb4ef4a38e8a94e
Rahul kumar <karn.rahulkumar14@gmail.com>
2025-10-07 07:57:47 +0530

    Update Register.jsx

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -24,7 +24,7 @@ const Register = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch("http://localhost:8080/api/auth/register", {
+      const response = await fetch("https://pujamoneycollection.vercel.app/api/auth/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
